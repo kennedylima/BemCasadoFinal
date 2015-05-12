c063ceefc2f4996b26e1afdabee6bca7eb9210b2
Kennedylima <kennedyoliveiralima@gmail.com>
2015-05-12 15:33:59 -0400

    Atualização do valor dos itens com arredondamento. Metodo de excluir itens da tabela. FALTA SALVAR OS ITENS NO BANCO Alterações feitas na table view e arquivo css. Kennedy

--- src/br/com/larimaia/util/ConexaoUtil.java
+++ src/br/com/larimaia/util/ConexaoUtil.java
@@ -11,7 +11,7 @@ public class ConexaoUtil {
     public static Connection getConnection() {
         String url = "jdbc:postgresql://localhost:5432/LariMaia";
         String usuario = "postgres";
-        String senha = "root";
+        String senha = "postgres";
         try {
             Connection connection = DriverManager.getConnection(url,usuario, senha);
             System.out.println("Conectado com Sucesso !!");
